db71aa26c3f9221a279af5c3eb34c49fdd83adee
James Fletcher <james@grakn.ai>
2020-02-10 16:04:08 +0000

    Refactor the Birth Agent by breaking it down

--- agents/PersonBirthAgent.java
+++ agents/PersonBirthAgent.java
@@ -15,64 +15,77 @@ public class PersonBirthAgent implements CityAgent {
 
     private static final int NUM_BIRTHS = 5;
 
+    private Random random;
+    private LocalDateTime dateToday;
+    private List<String> femaleForenames;
+    private List<String> maleForenames;
+    private List<String> surnames;
+    private World.City city;
+    private AgentContext context;
+
     @Override
     public void iterate(AgentContext context, RandomSource randomSource, World.City city) {
         city.log("-- Person Birth Agent --");
-        List<String> femaleForenames = context.getWorld().getFemaleForenames();
-        List<String> maleForenames = context.getWorld().getMaleForenames();
-        List<String> surnames = context.getWorld().getSurnames();
+        this.city = city;
+        this.context = context;
+        femaleForenames = context.getWorld().getFemaleForenames();
+        maleForenames = context.getWorld().getMaleForenames();
+        surnames = context.getWorld().getSurnames();
+        dateToday = context.getLocalDateTime();
+        random = randomSource.startNewRandom();
 
         String sessionKey = city.getCountry().getContinent().getName();
         GraknClient.Session session = context.getIterationGraknSessionFor(sessionKey);
 
         // Find bachelors and bachelorettes who are considered adults and who are not in a marriage and pair them off randomly
         try ( Transaction tx = session.transaction().write()) {
-            Random random = randomSource.startNewRandom();
-            LocalDateTime dateToday = context.getLocalDateTime();
-
             for (int i = 0; i < NUM_BIRTHS; i++) {
-                String gender;
-                String forename;
-                String surname = surnames.get(random.nextInt(surnames.size()));
-
-                boolean genderBool = random.nextBoolean();
-                if (genderBool) {
-                    gender = "male";
-                    forename = maleForenames.get(random.nextInt(maleForenames.size()));
-                } else {
-                    gender = "female";
-                    forename = femaleForenames.get(random.nextInt(femaleForenames.size()));
-                }
-
-                // Email is used as a key and needs to be unique, which requires a lot of information
-                String email = forename + "."
-                        + surname + "_"
-                        + dateToday.toString() + "_"
-                        + i + "_"
-                        + context.getSimulationStep() + "_"
-                        + city.getName() + "_"
-                        + city.getCountry().getName() + "_"
-                        + city.getCountry().getContinent().getName()
-                        + "@gmail.com";
-
-                GraqlInsert query =
-                        Graql.match(
-                                Graql.var("c").isa("city")
-                                        .has("name", city.getName()))
-                                .insert(Graql.var("p").isa("person")
-                                                .has("email", email)
-                                                .has("date-of-birth", dateToday)
-                                                .has("gender", gender)
-                                                .has("forename", forename)
-                                                .has("surname", surname),
-                                        Graql.var("b").isa("born-in")
-                                                .rel("born-in_child", "p")
-                                                .rel("born-in_place-of-birth", "c")
-                                );
-                city.logQuery(query);
-                List<ConceptMap> answers = tx.execute(query);
+                insertPerson(tx, i);
             }
             tx.commit();
         }
+    }
+
+    private void insertPerson(Transaction tx, int i) {
+        String gender;
+        String forename;
+        String surname = surnames.get(random.nextInt(surnames.size()));
+
+        boolean genderBool = random.nextBoolean();
+        if (genderBool) {
+            gender = "male";
+            forename = maleForenames.get(random.nextInt(maleForenames.size()));
+        } else {
+            gender = "female";
+            forename = femaleForenames.get(random.nextInt(femaleForenames.size()));
+        }
+
+        // Email is used as a key and needs to be unique, which requires a lot of information
+        String email = forename + "."
+                + surname + "_"
+                + dateToday.toString() + "_"
+                + i + "_"
+                + context.getSimulationStep() + "_"
+                + city.getName() + "_"
+                + city.getCountry().getName() + "_"
+                + city.getCountry().getContinent().getName()
+                + "@gmail.com";
+
+        GraqlInsert query =
+                Graql.match(
+                        Graql.var("c").isa("city")
+                                .has("name", city.getName()))
+                        .insert(Graql.var("p").isa("person")
+                                        .has("email", email)
+                                        .has("date-of-birth", dateToday)
+                                        .has("gender", gender)
+                                        .has("forename", forename)
+                                        .has("surname", surname),
+                                Graql.var("b").isa("born-in")
+                                        .rel("born-in_child", "p")
+                                        .rel("born-in_place-of-birth", "c")
+                        );
+        city.logQuery(query);
+        tx.execute(query);
     }
 }
