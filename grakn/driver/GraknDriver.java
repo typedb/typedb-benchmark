ca835db954ce05fb558ce0eb9d0977ba58d221ea
Ganeshwara Hananda <ganeshwara@grakn.ai>
2021-01-26 20:02:44 +0000

    Grakn Cluster - wip

--- grakn/driver/GraknDriver.java
+++ grakn/driver/GraknDriver.java
@@ -33,7 +33,7 @@ public class GraknDriver extends TransactionalDbDriver<Grakn.Transaction, Grakn.
     private final ConcurrentHashMap<String, Grakn.Session> sessionMap = new ConcurrentHashMap<>();
 
     public GraknDriver(String hostUri, String database) {
-        this.client = GraknClient.core(hostUri);
+        this.client = GraknClient.cluster(hostUri);
         this.database = database;
     }
 
