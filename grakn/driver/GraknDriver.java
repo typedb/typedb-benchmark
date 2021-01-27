deb04491e5bde08d5dd238b68749b7d6a50d672d
Ganeshwara Hananda <ganeshwara@grakn.ai>
2021-01-27 18:50:27 +0000

    Override host

--- grakn/driver/GraknDriver.java
+++ grakn/driver/GraknDriver.java
@@ -33,6 +33,7 @@ public class GraknDriver extends TransactionalDbDriver<Grakn.Transaction, Grakn.
     private final ConcurrentHashMap<String, Grakn.Session> sessionMap = new ConcurrentHashMap<>();
 
     public GraknDriver(String hostUri, String database) {
+        System.out.println("------  " + hostUri);
         this.client = GraknClient.cluster(hostUri);
         this.database = database;
     }
