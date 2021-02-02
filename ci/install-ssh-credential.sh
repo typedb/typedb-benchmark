5b3d66fc4d3d6f4c5ff59f3008a4f1efe7a83fe1
Ganeshwara Hananda <ganeshwara@grakn.ai>
2021-02-02 20:40:01 +0000

    Rename env var

--- ci/install-ssh-credential.sh
+++ ci/install-ssh-credential.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-bash -c 'echo $BENCHMARK_PRIVATE_KEY | base64 -d > ~/.ssh/id_rsa'
+bash -c 'echo $GRABL_JOB_PRIVATE_KEY | base64 -d > ~/.ssh/id_rsa'
 chmod 400 ~/.ssh/id_rsa
-bash -c 'echo $BENCHMARK_PUBLIC_KEY | base64 -d >> ~/.ssh/authorized_keys'
+bash -c 'echo $GRABL_JOB_PUBLIC_KEY | base64 -d >> ~/.ssh/authorized_keys'
