0697a88e9ce0ed8fb8247eb8c5ad74d04daf0d48
Ganeshwara Hananda <ganeshwara@grakn.ai>
2021-02-02 18:00:05 +0000

    Flesh out wait-for function

--- ci/wait-for.sh
+++ ci/wait-for.sh
@@ -2,4 +2,16 @@
 
 set -ex
 
-until [ -f "${1}" ]; do echo "Waiting for file ${1} to be created."; sleep 1; done
+max_attempts=100
+attempt_counter=0
+
+until [ -f "${1}" ]; do
+    if [ ${attempt_counter} -eq ${max_attempts} ]; then
+      echo "Error: max attempt of $max_attempts reached."
+      exit 1
+    fi
+
+    echo "Waiting for file ${1} has been created (attempt $attempt_counter).";
+    attempt_counter=$(($attempt_counter+1))
+    sleep 3;
+done
