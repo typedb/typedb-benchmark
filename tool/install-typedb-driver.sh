305196ac4b2c4a0296e02ea5ce1b69920894518e
Ganeshwara Hananda <ganesh@vaticle.com>
2024-12-04 18:44:14 +0000

    Add start/stop typedb script

--- tool/install-typedb-driver.sh
+++ tool/install-typedb-driver.sh
@@ -7,4 +7,3 @@ VERSION="3.0.0a7"
 sudo apt update -y
 sudo apt install -y python3-pip
 pip install typedb-driver=="$VERSION"
-python3 tpcc/pytpcc/tpcc.py --no-execute --scalefactor=100 --warehouses=4 --debug typedb3
