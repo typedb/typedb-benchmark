8e9e74105100e3c312cc35d338fe1a982a7d5244
Ganeshwara Hananda <ganesh@vaticle.com>
2024-12-06 17:13:08 +0000

    Convert sed syntax from Mac to Linux-compliant

--- tool/update-typedb-core-version.sh
+++ tool/update-typedb-core-version.sh
@@ -2,4 +2,4 @@ set -eu
 
 VERSION_VALUE=$1
 CONFIG_FILE="tool/config.sh"
-sed -i '' -E "s/^(SERVER_VERSION=).*/\1$VERSION_VALUE/" $CONFIG_FILE
+sed -i'' -E "s/^(SERVER_VERSION=).*/\1$VERSION_VALUE/" $CONFIG_FILE
