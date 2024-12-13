fe4e3345fc3dc0ca392234321b11797563d46d0f
Ganeshwara Hananda <ganesh@vaticle.com>
2024-12-13 16:01:56 +0000

    Update benchmark result output filename

--- tool/gcp/download-result.sh
+++ tool/gcp/download-result.sh
@@ -2,4 +2,4 @@ set -eu
 
 source ./tool/gcp/profile.sh
 
-gcloud compute scp --project=$PROJECT --zone=$ZONE $USER@$MACHINE_NAME:'~/typedb-benchmark/results.log' $MACHINE_NAME
+gcloud compute scp --project=$PROJECT --zone=$ZONE $USER@$MACHINE_NAME:'~/typedb-benchmark/results.log' result-$MACHINE_NAME.log
