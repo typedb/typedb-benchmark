d41a1bf7869a6aba32ed73d87efeaef5cf5fbbb5
Ganeshwara Hananda <ganesh@vaticle.com>
2024-12-16 16:34:01 +0000

    Fix TypeDB 3.0 benchmark CI

--- tool/typedb3/install-server.sh
+++ tool/typedb3/install-server.sh
@@ -2,7 +2,7 @@ set -eu
 
 source tool/typedb3/config.sh
 
-DISTRIBUTION="typedb-all-mac-arm64"
+DISTRIBUTION="typedb-all-linux-x86_64"
 DISTRIBUTION_DIR="$DISTRIBUTION-$SERVER_VERSION"
 DISTRIBUTION_TARGZ="$DISTRIBUTION_DIR.tar.gz"
 DISTRIBUTION_URL="https://repo.typedb.com/public/public-snapshot/raw/names/$DISTRIBUTION/versions/$SERVER_VERSION/$DISTRIBUTION_TARGZ"
