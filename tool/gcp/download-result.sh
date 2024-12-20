e552e77d53a53d2f58050a579a17251de69551c7
Ganeshwara Hananda <ganesh@vaticle.com>
2024-12-20 11:25:31 +0000

    Collect TypeDB Core server log, refactor profile.sh

--- tool/gcp/download-result.sh
+++ tool/gcp/download-result.sh
@@ -3,3 +3,4 @@ set -eu
 source ./tool/gcp/profile.sh
 
 gcloud compute scp --project=$PROJECT --zone=$ZONE $USER@$MACHINE_NAME:'~/typedb-benchmark/results.log' result-$MACHINE_NAME.log
+gcloud compute scp --project=$PROJECT --zone=$ZONE $USER@$MACHINE_NAME:"~/typedb-benchmark/typedb-all-linux-x86_64-$SERVER_VERSION/log" result-$MACHINE_NAME-server.log
