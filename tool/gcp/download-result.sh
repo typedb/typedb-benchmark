5820c4785d8a0320f9eb1403681376109d9d74e1
Ganeshwara Hananda <ganesh@vaticle.com>
2024-12-20 12:37:20 +0000

    Resolve typedb-folder more simply using glob '*' in the benchmark script

--- tool/gcp/download-result.sh
+++ tool/gcp/download-result.sh
@@ -3,4 +3,6 @@ set -eu
 source ./tool/gcp/profile.sh
 
 gcloud compute scp --project=$PROJECT --zone=$ZONE $USER@$MACHINE_NAME:'~/typedb-benchmark/results.log' result-$MACHINE_NAME.log
-gcloud compute scp --project=$PROJECT --zone=$ZONE $USER@$MACHINE_NAME:"~/typedb-benchmark/typedb-all-linux-x86_64-$SERVER_VERSION/log" result-$MACHINE_NAME-server.log
+
+# resolve typedb folder using glob '*', as there's exactly one typedb folder
+gcloud compute scp --project=$PROJECT --zone=$ZONE $USER@$MACHINE_NAME:"~/typedb-benchmark/typedb-all-linux-x86_64-*/log" result-$MACHINE_NAME-server.log
