1a53f1f3f2a1d5edb2e99e4e9704672e37670697
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2025-04-07 12:41:44 +0100

    extra index in pip

--- tool/typedb3/install-driver.sh
+++ tool/typedb3/install-driver.sh
@@ -4,4 +4,4 @@ source tool/typedb3/config.sh
 
 sudo apt update -y
 sudo apt install -y python3-pip
-pip install typedb-driver=="$DRIVER_VERSION" --index-url https://repo.typedb.com/public/public-snapshot/python/simple/
+pip install typedb-driver=="$DRIVER_VERSION" --extra-index-url https://repo.typedb.com/public/public-snapshot/python/simple/
