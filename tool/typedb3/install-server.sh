58afd060596d417c5114512fc13d27a7864855d4
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2025-04-10 09:38:31 +0100

    two tdb3 download paths

--- tool/typedb3/install-server.sh
+++ tool/typedb3/install-server.sh
@@ -5,7 +5,8 @@ source tool/typedb3/config.sh
 DISTRIBUTION="typedb-all-linux-x86_64"
 DISTRIBUTION_DIR="$DISTRIBUTION-$SERVER_VERSION"
 DISTRIBUTION_TARGZ="$DISTRIBUTION_DIR.tar.gz"
-DISTRIBUTION_URL="https://repo.typedb.com/public/public-snapshot/raw/names/$DISTRIBUTION/versions/$SERVER_VERSION/$DISTRIBUTION_TARGZ"
+DISTRIBUTION_URL_SNAPSHOT="https://repo.typedb.com/public/public-snapshot/raw/names/$DISTRIBUTION/versions/$SERVER_VERSION/$DISTRIBUTION_TARGZ"
+DISTRIBUTION_URL_RELEASE="https://repo.typedb.com/public/public-release/raw/names/$DISTRIBUTION/versions/$SERVER_VERSION/$DISTRIBUTION_TARGZ"
 
-curl -o "$DISTRIBUTION_TARGZ" "$DISTRIBUTION_URL"
-tar -xf "$DISTRIBUTION_TARGZ"
+(curl -o "$DISTRIBUTION_TARGZ" "$DISTRIBUTION_URL_SNAPSHOT" && tar -xf "$DISTRIBUTION_TARGZ") ||
+    (curl -o "$DISTRIBUTION_TARGZ" "$DISTRIBUTION_URL_RELEASE" && tar -xf "$DISTRIBUTION_TARGZ")
