aeb2cc0f27c7148d7f9395be87081c50e0495f0a
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2025-05-09 18:36:13 +0100

    wait for machine creation

--- tool/ovh/create.sh
+++ tool/ovh/create.sh
@@ -5,3 +5,8 @@ openstack server create "$MACHINE_NAME" \
     --flavor "$MACHINE_TYPE" \
     --image "$IMAGE" \
     --network Ext-Net
+
+for _ in `seq 60`; do
+    STATUS=$(openstack server show "$MACHINE_NAME" |& grep '\bstatus' | awk '{print $4}')
+    if [[ $STATUS == "ACTIVE" ]]; then break; fi
+done
