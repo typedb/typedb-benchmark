a22369570236d31f4cf1137f713f75a386e21dc6
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2025-05-12 09:36:45 +0100

    replace scp with ssh

--- tool/ovh/download-result.sh
+++ tool/ovh/download-result.sh
@@ -1,6 +1,6 @@
-gcloud compute scp --project=$PROJECT --zone=$ZONE $USER@$MACHINE_NAME:'~/typedb-benchmark/results.log' result-$MACHINE_NAME.log
+openstack server ssh $MACHINE_NAME -4 --address-type Ext-Net -- -i $OS_PRIVATE_KEY -l ubuntu -o StrictHostKeychecking=no "cat ~/typedb-benchmark/results.log" > result-$MACHINE_NAME.log
 # resolve typedb folder using glob '*', as there's exactly one typedb folder
 if [[ $DB =~ typedb ]]; then
-    gcloud compute scp --project=$PROJECT --zone=$ZONE $USER@$MACHINE_NAME:"~/typedb-benchmark/typedb-all-linux-x86_64-*/log" result-$MACHINE_NAME-server.log
+    openstack server ssh $MACHINE_NAME -4 --address-type Ext-Net -- -i $OS_PRIVATE_KEY -l ubuntu -o StrictHostKeychecking=no "cat ~/typedb-benchmark/typedb-all-linux-x86_64-*/log" > result-$MACHINE_NAME-server.log
 fi
 
