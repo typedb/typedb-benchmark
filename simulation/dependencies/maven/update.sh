a5fd082e1f6eb08239d6e44fb724a7f5124b8b86
Krishnan Govindraj <krishnan@typedb.com>
2025-07-11 21:56:13 +0530

    Is updating simulation gonna work?

--- simulation/dependencies/maven/update.sh
+++ simulation/dependencies/maven/update.sh
@@ -16,4 +16,4 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-bazel run @vaticle_dependencies//library/maven:update
+bazel run @typedb_dependencies//library/maven:update
