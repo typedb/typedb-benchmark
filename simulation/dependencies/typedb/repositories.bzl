b22dbc34c9628cb0b16520592b8eebc3d3baa78f
Krishnan Govindraj <krishnan@typedb.com>
2025-07-11 23:01:49 +0530

    simulation & iam now build

---
+++ simulation/dependencies/typedb/repositories.bzl
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 Vaticle
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+load("@bazel_tools//tools/build_defs/repo:git.bzl", "git_repository")
+
+def typedb_dependencies():
+    git_repository(
+        name = "typedb_dependencies",
+        remote = "https://github.com/typedb/dependencies",
+        commit = "fac1121c903b0c9e5924d391a883e4a0749a82a2",  # sync-marker: do not remove this comment, this is used for sync-dependencies by @typedb_dependencies
+    )
--- simulation/framework/typedb/TypeDBClient.kt
+++ simulation/framework/typedb/TypeDBClient.kt
@@ -25,6 +25,7 @@ import com.vaticle.typedb.driver.api.TypeDBSession.Type.DATA
 import com.vaticle.typedb.driver.api.TypeDBTransaction.Type.READ
 import com.vaticle.typeql.lang.TypeQL.match
 import com.vaticle.typeql.lang.TypeQL.cVar
+import com.vaticle.typeql.lang.common.TypeQLToken
 import java.text.DecimalFormat
 import java.util.concurrent.ConcurrentHashMap
 
@@ -47,10 +48,10 @@ class TypeDBDriver private constructor(
         nativeClient.session(database, DATA).use { session ->
             session.transaction(READ).use { tx ->
                 val formatter = DecimalFormat("#,###")
-                val numberOfEntities = tx.query().get(match(cVar("x").isa("entity")).count()).get().asLong()
-                val numberOfAttributes = tx.query().get(match(cVar("x").isa("attribute")).count()).get().asLong()
-                val numberOfRelations = tx.query().get(match(cVar("x").isa("relation")).count()).get().asLong()
-                val numberOfThings = tx.query().get(match(cVar("x").isa("thing")).count()).get().asLong()
+                val numberOfEntities = tx.query().get(match(cVar("x").isa("entity")).get().aggregate(TypeQLToken.Aggregate.Method.COUNT, cVar("x"))).resolve().get().asLong()
+                val numberOfAttributes = tx.query().get(match(cVar("x").isa("attribute")).get().aggregate(TypeQLToken.Aggregate.Method.COUNT, cVar("x"))).resolve().get().asLong()
+                val numberOfRelations = tx.query().get(match(cVar("x").isa("relation")).get().aggregate(TypeQLToken.Aggregate.Method.COUNT, cVar("x"))).resolve().get().asLong()
+                val numberOfThings = tx.query().get(match(cVar("x").isa("thing")).get().aggregate(TypeQLToken.Aggregate.Method.COUNT, cVar("x"))).resolve().get().asLong()
                 str.append("Simulation statistic:").append("\n")
                 str.append("\n")
                 str.append("Count 'entity': ").append(formatter.format(numberOfEntities)).append("\n")
